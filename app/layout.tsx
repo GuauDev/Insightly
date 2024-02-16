ff382e5485517fe8bc9113878028d66c36923f72
Daniel fernando reyes v <dfreyes1236@gmail.com>
2024-02-16 17:36:46 -0500

    Upload dark mode and insstall shadcn 🗂️

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { GeistSans } from "geist/font/sans";
+import { ThemeProvider } from "@/components/theme-provider";
+
 import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
@@ -17,12 +19,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
-      </body>
-    </html>
-  );
+		<html lang="en" className={GeistSans.className}>
+			<body className="bg-background text-foreground">
+				<ThemeProvider attribute="class" defaultTheme="system">
+					<main className="min-h-screen flex flex-col items-center">
+						{children}
+					</main>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
